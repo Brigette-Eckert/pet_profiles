8907d4bb7be99008e2444fef36939fd7d9112423
Brigette Eckert <brigette.eckert@live.com>
2016-06-16 01:36:49 -0700

    created superuser, set up human and pet models and registered them with admin

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Human(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField()
+    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    location = models.CharField(max_length=255)
+    bio = models.TextField()
+
+#look into pillow for Image Field
+#fix defaults--djmake && djrun not working
+
+class Pet(models.Model):
+    human = models.ForeignKey(Human)
+    name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    age = models.PositiveSmallIntegerField()
+    picture = models.ImageField()
+    hobbies = models.CharField(max_length=1000)
+
+
