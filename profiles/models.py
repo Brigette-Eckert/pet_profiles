1654696db6b8df810156eded792eb5a47cc4146d
Brigette Eckert <brigette.eckert@live.com>
2016-06-17 01:30:57 -0700

    udpdated models to include avatar upload and figuring out datetime.  Set up layout html, and css files

--- profiles/models.py
+++ profiles/models.py
@@ -1,25 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
-
 class Human(models.Model):
     user = models.OneToOneField(User)
-    avatar = models.ImageField()
-    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    avatar = models.ImageField(upload_to=None)
+    birthday = models.DateField(auto_now=True, editable=True)
     location = models.CharField(max_length=255)
     bio = models.TextField()
 
-#look into pillow for Image Field
-#fix defaults--djmake && djrun not working
 
 class Pet(models.Model):
     human = models.ForeignKey(Human)
     name = models.CharField(max_length=255)
     nickname = models.CharField(max_length=255)
     age = models.PositiveSmallIntegerField()
-    picture = models.ImageField()
+    picture = models.ImageField(upload_to=None)
     hobbies = models.CharField(max_length=1000)
 
 
