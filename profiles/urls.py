5c7f76ada2734fbc93f0df6180cd4509c308b6a7
Brigette Eckert <brigette.eckert@live.com>
2016-06-20 14:00:26 -0700

    added a css palette, playing around with views and urls to figure out routing issues

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     # SINGULAR, the home route for the CURRENT user
-    url(r'^', views.profile_home, name="home"),
+    url(r'^$', views.profile_home, name="home"),
     # MULTIPLE, the profile page for ANY user
-    url(r'^profiles(?P<pk>\d+)', views.profile_display, name='display'),
+    url(r'^profiles(?P<pk>\d+)', views.profile_detail, name='detail'),
 ]
+
+
+#where do I tell it to run the function from view-
+# aka how does it know which function to run? aka how do I add profile_detail to view
--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,12 @@ from profiles.models import Human
 
 
 def profile_home(request):
+    return render(request, "profiles/profile_list.html", {"profile": {}})
+
+
+def profile_detail(request, pk):
     # check if the user is logged in. user is in "request.user"
-    # profile = Human.objects.get(user=request.user) # need to set the pk to the current user pk
-    return render(request, "profiles/profile_detail.html", {"profile": {}})
-
-
-def profile_display(request, pk):
+    # profile = Human.objects.get(user=request.user)
     profile = get_object_or_404(Human, pk=pk)
-    return None # return a new template, profile_display, send it "profile" in a kwarg
+    return render(request, "profiles/profile_detail.html", {"profile": {}})
 
