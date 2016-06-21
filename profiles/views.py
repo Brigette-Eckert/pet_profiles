b9e12f41188e6a909cbcee492762acace9a450b2
Brigette Eckert <brigette.eckert@live.com>
2016-06-20 20:17:37 -0700

    changed views so that data is now showing up on landing page

--- profiles/views.py
+++ profiles/views.py
@@ -4,14 +4,13 @@ from profiles.models import Human, Pet
 
 
 def profile_home(request):
-    profiles = Human.objects.all()
-    return render(request, "profiles/profile_list.html", {"profiles": {profiles}})
+    human = Human.objects.all()
+    pet = Pet.objects.all()
+    return render(request, "profiles/profile_list.html", {"human": human, "pet": pet})
 
+#template not loading  anymore
 
 def profile_detail(request, pk):
-    # check if the user is logged in. user is in "request.user"
-    human = Human.objects.get(user=request.user)
-    profile = get_object_or_404(Human, pk=pk)#add cat 404 error
-    return render(request, "profiles/profile_detail.html", {"profile": {profile}})
-
-# need to pass in models- in the dicts- so it can find class.atrribute
+    #check if user is logged in, if so show, if not take to login/signup screen
+    profile = get_object_or_404(Human, pk=pk)
+    return render(request, "profiles/profile_detail.html", {"profile": profile})
